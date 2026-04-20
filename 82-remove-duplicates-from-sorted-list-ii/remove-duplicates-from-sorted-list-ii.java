63fa9d17cf97d2bf7ba7a818c9533bd4ac41b50f
shaquibhassan1702 <shaquib.hasan.cs27@iilm.edu>
2026-04-20 14:24:31 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (87.39%) - LeetSync

---
+++ 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        
+        ListNode sentinel = new ListNode(0, head);
+        
+       
+        ListNode prev = sentinel;
+        
+        while (head != null) {
+           
+            if (head.next != null && head.val == head.next.val) {
+                
+                while (head.next != null && head.val == head.next.val) {
+                    head = head.next;
+                }
+                
+                prev.next = head.next;
+            } else {
+                
+                prev = prev.next;
+            }
+            
+            
+            head = head.next;
+        }
+        
+        return sentinel.next;
+    }
+}
