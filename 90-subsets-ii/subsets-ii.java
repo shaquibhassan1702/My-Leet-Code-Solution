8daedd434614881d7a10512b24abd320e7e3807c
shaquibhassan1702 <shaquib.hasan.cs27@iilm.edu>
2026-04-20 09:43:16 +0530

    Time: 3 ms (56.43%) | Memory: 45.3 MB (12.98%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums); // important!
+        
+        backtrack(0, nums, new ArrayList<>(), result);
+        return result;
+    }
+
+    private void backtrack(int start, int[] nums, List<Integer> current, 
+                           List<List<Integer>> result) {
+        
+        result.add(new ArrayList<>(current));
+
+        for (int i = start; i < nums.length; i++) {
+            // skip duplicates
+            if (i > start && nums[i] == nums[i - 1]) continue;
+
+            current.add(nums[i]);
+            backtrack(i + 1, nums, current, result);
+            current.remove(current.size() - 1); // backtrack
+        }
+    }
+}
