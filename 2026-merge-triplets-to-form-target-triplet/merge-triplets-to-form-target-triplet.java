d6c61419ccd61006a25f256d05a48115f0cccb3e
shaquibhassan1702 <shaquib.hasan.cs27@iilm.edu>
2026-09-01 10:15:56 +0530

    Time: 5 ms (72.84%) | Memory: 166.5 MB (99.32%) - LeetSync

---
+++ 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean mergeTriplets(int[][] triplets, int[] target) {
+        boolean x=false;
+        boolean y=false;
+        boolean z=false;
+
+        for(int[] t:triplets){ 
+            
+            if(t[0]>target[0]|| t[1]>target[1]|| t[2]>target[2]){
+                continue;
+            }
+            if(t[0]==target[0])x=true;
+            if(t[1]==target[1])y=true;
+            if(t[2]==target[2])z=true;
+        }
+        return x && y && z;
+    }
+}
